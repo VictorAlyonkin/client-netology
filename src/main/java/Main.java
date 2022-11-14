48350e176761650a137a642183d12286f3934f53
Victor Alyonkin <viktoalenkin@ya.ru>
2022-11-14 14:45:18 +0300

    update host "netology"

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,19 +5,23 @@ import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Main {
-    public static void main(String[] args)  {
-        String host = "127.0.0.1";
+    public static void main(String[] args) {
+        String host = "netology.homework";
         int port = 8086;
         try (Socket clientSocket = new Socket(host, port);
              PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
              BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
 
-            out.println("Victor");
-
             String reps = in.readLine();
             System.out.println(reps);
+            out.println("Victor");
+            reps = in.readLine();
+            System.out.println(reps);
+            out.println("no");
+            reps = in.readLine();
+            System.out.println(reps);
 
-        } catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
